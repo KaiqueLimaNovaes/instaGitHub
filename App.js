8ee178a8c7591ebfd4eb2292b99c9ceb30026f5a
KaiqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2020-12-28 12:18:05 -0300

    primeiro

--- App.js
+++ App.js
@@ -1,12 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import imagem from './astronautagame.png';
+
+function InstaGitHubCard({githubUser}){
+  return(
+    <View>
+      {/*instaCardHeader*/}
+      <View>
+        <Text>{githubUser}</Text>
+      </View>
+      {/*instaCardHeader*/}
+      
+      {/*instaCardImage*/}
+      <View>
+        <Image
+          style={{
+            width: 60,
+            height: 60,
+            borderRadius: 30,
+          }}
+          source={{uri:`https://github.com/${githubUser}.png`}}
+        />
+      </View>
+      
+      <Image
+        style={{
+          width: '100%',
+          height: 350,
+        }}
+        source={{uri:`https://github.com/${githubUser}.png`}}
+      />
+      {/*instaCardImage*/}
+
+      {/*footer*/}
+      <View>
+        <Text>
+          <AntDesign name="hearto" size={24} color="black" />
+        </Text>
+      </View>
+      {/*footer*/}
+    </View>
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView>
+        {
+          [
+            'KaiqueLimaNovaes',
+            'elvis-brito',
+            'renatazadravec',
+            'emersonope',
+            'SergioAmarante',
+            'LeandroSoares31'
+          ].map((githubUser) => <InstaGitHubCard key={githubUser} githubUser={githubUser} />)
+        }
+      </ScrollView>
     </View>
   );
 }
@@ -14,8 +67,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    //alignItems: 'center',
+    //justifyContent: 'center',
   },
 });
